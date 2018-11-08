0ebb7a6d27e71ab52c471ff9ec91b999665322d8
Daniel Hristov <dhristov@live.co.uk>
2018-11-08 11:35:44 +0100

    refactoring to make code cleaner - still more work needed

---
+++ minesweeper/js/Cell.js
@@ -0,0 +1,97 @@
+"use strict";
+const ctx = document.getElementById("canvas").getContext("2d");
+const icons = {
+  back: "/minesweeper/icons/back.png",
+  flag: "/minesweeper/icons/flag.png",
+  mine: "/minesweeper/icons/mine.png",
+  0: "/minesweeper/icons/0.png",
+  1: "/minesweeper/icons/1.png",
+  2: "/minesweeper/icons/2.png",
+  3: "/minesweeper/icons/3.png",
+  4: "/minesweeper/icons/4.png",
+  5: "/minesweeper/icons/5.png",
+  6: "/minesweeper/icons/6.png",
+  7: "/minesweeper/icons/7.png",
+  8: "/minesweeper/icons/8.png"
+};
+
+const CellFactory = state => {
+  let x = state.point.x;
+  let y = state.point.y;
+  let point = state.point;
+  let bomb = state.bomb;
+  let bombCount = 0;
+  let isOpen = false;
+  let w = state.w;
+  let h = state.h;
+  let drawImage = img => {
+    ctx.drawImage(img, x, y);
+  };
+  let fImage = new Image(w, h);
+  fImage.onload = function() {
+    drawImage(this);
+  };
+  fImage.src = icons.back;
+  let bImage = new Image(w, h);
+  bImage.onload = function() {
+    drawImage(this);
+  };
+  let markedBomb = false;
+
+  return {
+    //public functions here
+    increaseBombCount() {
+      bombCount++;
+    },
+
+    markAsBomb() {
+      if (!isOpen) {
+        markedBombs = markedBombs + (markedBomb ? -1 : 1);
+        markedBomb = !markedBomb;
+        bImage.src = markedBomb ? icons.flag : icons.back;
+        setBombCount();
+      }
+    },
+
+    getBombCount() {
+      return bombCount;
+    },
+
+    getPoint() {
+      return point;
+    },
+
+    isBomb() {
+      return bomb;
+    },
+
+    isOpen() {
+      return isOpen;
+    },
+
+    isMarked() {
+      return markedBomb;
+    },
+
+    showIcon() {
+      bImage.src = bomb ? icons.mine : icons[bombCount];
+    },
+
+    open() {
+      if (!isOpen) {
+        isOpen = true;
+        openedCells++;
+        this.showIcon();
+        if (bomb) {
+          //game over :(
+          stopTimer();
+          showAllBombs();
+          gameOver();
+        } else if (bombCount == 0) {
+          //open all neighbours which are not bombs
+          openEmptyNeighbours(point);
+        }
+      }
+    }
+  };
+};
--- minesweeper/js/minesweeper.js
+++ minesweeper/js/minesweeper.js
@@ -1,250 +1,336 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-const cell_width = (cell_height = 40);
+"use strict";
+//Refactoring in progress things might break at times :) :/
+const cell_width = 40;
+const cell_height = 40;
 const difficulties = {
   easy: 2.5,
   medium: 4,
-  hard: 6
+  hard: 5.5
+};
+const leftClick = 0;
+const rightClick = 2;
+//maybe add jquery to simplify some stuff ?
+const timer = document.getElementById("timer");
+const bombCount = document.getElementById("bombs");
+const canvas = document.getElementById("canvas");
+const mouse = {
+  left: false,
+  right: false
 };
 
-var timer = document.getElementById("timer");
+//Could make this an arrow function
+//and use it as a class and avoid resetting everything manually
+//by just creating a new object every time ...
+//This will also allow for the creation of private variables instead
+//of the current public ones (even if they are within the game const).
+//This will be next step of refactoring...
+const game = {
+  difficulty: "easy",
+  timerInterval: null,
+  startDate: null,
+  cells: null,
+  openedCells: null,
+  bombs: 0,
+  markedBombs: 0,
 
-var difficulty = "easy";
-var timerInterval = null;
-var startDate = null;
+  getDifficulty: () => {
+    return this.difficulty;
+  },
 
-var cells = null;
-var openedCells = 0;
-var bombs = 0;
-var markedBombs = 0;
-var bombCount = document.getElementById("bombs");
+  setDifficulty: difficulty => {
+    this.difficulty = difficulty;
+  },
 
-class Cell {
-  constructor(bomb, point) {
-    this.bomb = bomb;
-    this.bombCount = 0;
-    this.isOpen = false;
-    this.fImage = new Image(cell_width, cell_height);
-    this.fImage.onload = function() {
-      ctx.drawImage(this, point.x, point.y);
-    };
-    this.fImage.src = "/minesweeper/icons/back.png";
-    this.bImage = new Image(cell_width, cell_height);
-    this.bImage.onload = function() {
-      ctx.drawImage(this, point.x, point.y);
-    };
-    this.point = point;
-    this.markedBomb = false;
+  getTimerInterval: () => {
+    return this.timerInterval;
+  },
+
+  setTimerInterval: timerInterval => {
+    this.timerInterval = timerInterval;
+  },
+
+  getStartDate: () => {
+    return this.startDate;
+  },
+
+  setStartDate: date => {
+    this.startDate = date;
+  },
+
+  getCells: () => {
+    return this.cells;
+  },
+
+  setCells: cells => {
+    this.cells = cells;
+  },
+
+  getOpenedCells: () => {
+    return this.openedCells;
+  },
+
+  setOpenedCells: openedCells => {
+    this.openedCells = openedCells;
+  },
+
+  getBombs: () => {
+    return this.bombs;
+  },
+
+  setBombs: bombs => {
+    this.bombs = bombs;
+  },
+
+  getMarkedBombs: () => {
+    return this.markedBombs;
+  },
+
+  setMarkedBombs: markedBombs => {
+    this.markedBombs = markedBombs;
   }
+};
 
-  increaseBombCount() {
-    this.bombCount++;
-  }
+/**
+ *
+ * @param {point, bomb, notBomb, notMarked, open} arg object contains
+ * the point of the cell and flags to determine which conditions to check
+ * for the given cell's neighbours
+ * and the action to take upon meeting that condition:
+ * bomb - check if bombs, notBomb - check if not bombs,
+ * notMarked - check if not marked,
+ * open - open the cell or if false or non existent, add 1 to mines count for that cell
+ */
+const iterateCellNeighbours = (arg = {}) => {
+  if (!arg) return;
+  const x = arg.point.x / cell_width;
+  const y = arg.point.y / cell_height;
+  const cells = game.getCells();
 
-  markAsBomb() {
-    if (!this.isOpen) {
-      markedBombs = markedBombs + (this.markedBomb ? -1 : 1);
-      this.markedBomb = !this.markedBomb;
-      this.bImage.src = this.markedBomb
-        ? "/minesweeper/icons/flag.png"
-        : "/minesweeper/icons/back.png";
-      setBombCount();
-    }
-  }
-
-  getBombCount() {
-    return this.bombCount;
-  }
-
-  getPoint() {
-    return this.point;
-  }
-
-  isBomb() {
-    return this.bomb;
-  }
-
-  isOpen() {
-    return this.isOpen;
-  }
-
-  showIcon() {
-    this.bImage.src = this.bomb
-      ? "/minesweeper/icons/mine.png"
-      : "/minesweeper/icons/" + this.bombCount + ".png";
-  }
-
-  open() {
-    if (!this.isOpen) {
-      this.isOpen = true;
-      openedCells++;
-      this.bImage.src = this.bomb
-        ? "/minesweeper/icons/mine.png"
-        : "/minesweeper/icons/" + this.bombCount + ".png";
-      if (this.bomb) {
-        //game over :(
-        stopTimer();
-        showAllBombs();
-        gameOver();
-      } else if (this.bombCount == 0) {
-        //open all neighbours which are not bombs
-        openEmptyNeighbours(this.point);
-      }
-    }
-  }
-}
-
-function openEmptyNeighbours(point) {
-  const x = point.x / 40;
-  const y = point.y / 40;
   if (cells[x - 1]) {
-    if (cells[x - 1][y - 1] && !cells[x - 1][y - 1].isBomb())
-      cells[x - 1][y - 1].open();
-    if (cells[x - 1][y] && !cells[x - 1][y].isBomb()) cells[x - 1][y].open();
-    if (cells[x - 1][y + 1] && !cells[x - 1][y + 1].isBomb())
-      cells[x - 1][y + 1].open();
+    if (
+      cells[x - 1][y - 1] &&
+      ((arg.notBomb && !cells[x - 1][y - 1].isBomb()) ||
+        (arg.notMarked && !cells[x - 1][y - 1].isMarked()) ||
+        (arg.bomb && cells[x - 1][y - 1].isBomb()))
+    )
+      arg.open ? cells[x - 1][y - 1].open() : cells[x][y].increaseBombCount();
+    if (
+      cells[x - 1][y] &&
+      ((arg.notBomb && !cells[x - 1][y].isBomb()) ||
+        (arg.notMarked && !cells[x - 1][y].isMarked()) ||
+        (arg.bomb && cells[x - 1][y].isBomb()))
+    )
+      arg.open ? cells[x - 1][y].open() : cells[x][y].increaseBombCount();
+    if (
+      cells[x - 1][y + 1] &&
+      ((arg.notBomb && !cells[x - 1][y + 1].isBomb()) ||
+        (arg.notMarked && !cells[x - 1][y + 1].isMarked()) ||
+        (arg.bomb && cells[x - 1][y + 1].isBomb()))
+    )
+      arg.open ? cells[x - 1][y + 1].open() : cells[x][y].increaseBombCount();
   }
-  if (cells[x][y - 1] && !cells[x][y - 1].isBomb()) cells[x][y - 1].open();
-  if (cells[x][y + 1] && !cells[x][y + 1].isBomb()) cells[x][y + 1].open();
+  if (
+    cells[x][y - 1] &&
+    ((arg.notBomb && !cells[x][y - 1].isBomb()) ||
+      (arg.notMarked && !cells[x][y - 1].isMarked()) ||
+      (arg.bomb && cells[x][y - 1].isBomb()))
+  )
+    arg.open ? cells[x][y - 1].open() : cells[x][y].increaseBombCount();
+  if (
+    cells[x][y + 1] &&
+    ((arg.notBomb && !cells[x][y + 1].isBomb()) ||
+      (arg.notMarked && !cells[x][y + 1].isMarked()) ||
+      (arg.bomb && cells[x][y + 1].isBomb()))
+  )
+    arg.open ? cells[x][y + 1].open() : cells[x][y].increaseBombCount();
 
   if (cells[x + 1]) {
-    if (cells[x + 1][y - 1] && !cells[x + 1][y - 1].isBomb())
-      cells[x + 1][y - 1].open();
-    if (cells[x + 1][y] && !cells[x + 1][y].isBomb()) cells[x + 1][y].open();
-    if (cells[x + 1][y + 1] && !cells[x + 1][y + 1].isBomb())
-      cells[x + 1][y + 1].open();
+    if (
+      cells[x + 1][y - 1] &&
+      ((arg.notBomb && !cells[x + 1][y - 1].isBomb()) ||
+        (arg.notMarked && !cells[x + 1][y - 1].isMarked()) ||
+        (arg.bomb && cells[x + 1][y - 1].isBomb()))
+    )
+      arg.open ? cells[x + 1][y - 1].open() : cells[x][y].increaseBombCount();
+    if (
+      cells[x + 1][y] &&
+      ((arg.notBomb && !cells[x + 1][y].isBomb()) ||
+        (arg.notMarked && !cells[x + 1][y].isMarked()) ||
+        (arg.bomb && cells[x + 1][y].isBomb()))
+    )
+      arg.open ? cells[x + 1][y].open() : cells[x][y].increaseBombCount();
+    if (
+      cells[x + 1][y + 1] &&
+      ((arg.notBomb && !cells[x + 1][y + 1].isBomb()) ||
+        (arg.notMarked && !cells[x + 1][y + 1].isMarked()) ||
+        (arg.bomb && cells[x + 1][y + 1].isBomb()))
+    )
+      arg.open ? cells[x + 1][y + 1].open() : cells[x][y].increaseBombCount();
   }
-}
+};
 
-function gameOver() {
+const openEmptyNeighbours = point => {
+  iterateCellNeighbours({
+    point: point,
+    notBomb: true,
+    open: true
+  });
+};
+
+const openNeighbours = point => {
+  iterateCellNeighbours({
+    point: point,
+    notMarked: true,
+    open: true
+  });
+};
+
+const calcMines = point => {
+  iterateCellNeighbours({
+    point: point,
+    bomb: true
+  });
+};
+
+const gameOver = () => {
   clearBoundEvents();
   alert("You lose :(");
-}
+};
 
-function gameWon() {
+const gameWon = () => {
   clearBoundEvents();
   alert("You win :)");
-}
+};
 
-function clearBoundEvents() {
+const clearBoundEvents = () => {
   canvas.onmousedown = null;
-}
+  canvas.onmouseup = null;
+};
 
-function setBombCount() {
-  bombCount.innerHTML = markedBombs;
-}
+const setBombCount = () => {
+  bombCount.innerHTML = game.getMarkedBombs();
+};
 
-function showAllBombs() {
+const showAllBombs = () => {
+  const cells = game.getCells();
   for (let i = 0; i < cells.length; i++) {
-    for (cell of cells[i]) {
-      if (cell.isBomb()) cell.showIcon();
+    for (let cell of cells[i]) {
+      if (cell.isBomb() && !cell.isMarked()) cell.showIcon();
     }
   }
-}
+};
 
-function drawGrid() {
+const drawGrid = () => {
+  let cells = game.getCells();
+  let bombs = game.getBombs();
   for (let i = 0; i <= 9; i++) {
     let sx = i * cell_width;
     for (let j = 0; j <= 9; j++) {
       let sy = j * cell_height;
       let rnd = Math.random() * 10 + 1;
-      let bomb = rnd < difficulties[difficulty];
-      cells[i].push(new Cell(bomb, { x: sx, y: sy }));
+      let bomb = rnd < difficulties[game.getDifficulty()];
+      cells[i].push(
+        CellFactory({
+          point: { x: sx, y: sy },
+          bomb: bomb,
+          w: cell_width,
+          h: cell_height
+        })
+      );
       if (bomb) bombs++;
     }
   }
-}
+  game.setCells(cells);
+  game.setBombs(bombs);
+};
 
-function calcMines(point) {
-  const x = point.x;
-  const y = point.y;
-  if (cells[x - 1]) {
-    if (cells[x - 1][y - 1] && cells[x - 1][y - 1].isBomb())
-      cells[x][y].increaseBombCount();
-    if (cells[x - 1][y] && cells[x - 1][y].isBomb())
-      cells[x][y].increaseBombCount();
-    if (cells[x - 1][y + 1] && cells[x - 1][y + 1].isBomb())
-      cells[x][y].increaseBombCount();
-  }
-  if (cells[x][y - 1] && cells[x][y - 1].isBomb())
-    cells[x][y].increaseBombCount();
-  if (cells[x][y + 1] && cells[x][y + 1].isBomb())
-    cells[x][y].increaseBombCount();
-
-  if (cells[x + 1]) {
-    if (cells[x + 1][y - 1] && cells[x + 1][y - 1].isBomb())
-      cells[x][y].increaseBombCount();
-    if (cells[x + 1][y] && cells[x + 1][y].isBomb())
-      cells[x][y].increaseBombCount();
-    if (cells[x + 1][y + 1] && cells[x + 1][y + 1].isBomb())
-      cells[x][y].increaseBombCount();
-  }
-}
-
-function initGame() {
-  cells = [[], [], [], [], [], [], [], [], [], []];
-  startDate = new Date();
-  bombs = 0;
-  markedBombs = 0;
-  openedCells = 0;
+const initGame = () => {
+  game.setCells([[], [], [], [], [], [], [], [], [], []]);
+  game.setStartDate(new Date());
+  game.setBombs(0);
+  game.setMarkedBombs(0);
+  game.setOpenedCells(0);
+  mouse.left = false;
+  mouse.right = false;
   setBombCount();
   drawGrid();
+  const cells = game.getCells();
   for (let i = 0; i < cells.length; i++) {
     for (let j = 0; j < cells[i].length; j++) {
-      calcMines({ x: i, y: j });
+      const point = { x: i * cell_width, y: j * cell_height };
+      calcMines(point);
     }
   }
   initCanvasEvents();
   startTimer();
-}
+};
 
-function startTimer() {
-  timerInterval = setInterval(() => {
-    elapsedTime = (new Date() - startDate) / 1000;
-    let secs =
-      "" + (elapsedTime % 60 < 10 ? "0" : "") + Math.floor(elapsedTime % 60);
-    let minutes = elapsedTime / 60;
-    let mins = "" + (minutes % 60 < 10 ? "0" : "") + Math.floor(minutes);
-    let hours = minutes / 60;
-    let hs = "" + (hours % 60 < 10 ? "0" : "") + Math.floor(hours);
-    timer.innerHTML = hs + ":" + mins + ":" + secs;
-  }, 999);
-}
+const startTimer = () => {
+  game.setTimerInterval(
+    setInterval(() => {
+      const elapsedTime = (new Date() - game.getStartDate()) / 1000;
+      let secs =
+        "" + (elapsedTime % 60 < 10 ? "0" : "") + Math.floor(elapsedTime % 60);
+      let minutes = elapsedTime / 60;
+      let mins = "" + (minutes % 60 < 10 ? "0" : "") + Math.floor(minutes);
+      let hours = minutes / 60;
+      let hs = "" + (hours % 60 < 10 ? "0" : "") + Math.floor(hours);
+      timer.innerHTML = hs + ":" + mins + ":" + secs;
+    }, 999)
+  );
+};
 
-function stopTimer() {
-  clearInterval(timerInterval);
-}
+const stopTimer = () => {
+  clearInterval(game.getTimerInterval());
+};
 
-function initCanvasEvents() {
-  canvas.onmousedown = function(evt) {
+const initCanvasEvents = () => {
+  canvas.onmouseup = function(evt) {
+    const cells = game.getCells();
     const padding = 46;
-    const cellSize = 40;
     const x = Math.floor(
-      (event.pageX - canvas.offsetLeft - padding) / cellSize
+      (event.pageX - canvas.offsetLeft - padding) / cell_width
     );
-    const y = Math.floor((event.pageY - canvas.offsetTop - padding) / cellSize);
+    const y = Math.floor(
+      (event.pageY - canvas.offsetTop - padding) / cell_height
+    );
     if (x < 0 || x > cells.length - 1 || y < 0 || y > cells.length - 1)
       return false;
-    //get mouse button to determine left or right click
+
     const mouseClick = evt.button;
-    const leftClick = 0;
-    //   const rightClick = 2;
-    if (mouseClick == leftClick) {
+    if (mouseClick == leftClick || (mouseClick == rightClick && mouse.left)) {
+      if (mouse.right) {
+        openNeighbours(cells[x][y].getPoint());
+      }
       cells[x][y].open();
       //Check if there are unopened cells
-      if (openedCells + markedBombs >= 100 || openedCells + bombs >= 100) {
+      const openedCells = game.getOpenedCells();
+      if (
+        openedCells + game.getMarkedBombs() >= 100 ||
+        openedCells + game.getBombs() >= 100
+      ) {
         stopTimer();
         gameWon();
       }
-    } else {
+    } else if (mouseClick == rightClick) {
       cells[x][y].markAsBomb();
     }
+    resetClicks();
   };
-}
+  canvas.onmousedown = function(evt) {
+    if (evt.button == leftClick) mouse.left = true;
+    else if (evt.button == rightClick) mouse.right = true;
+  };
+};
 
-function changeDifficulty(button) {
-  difficulty = button.innerHTML.toLocaleLowerCase();
+const resetClicks = () => {
+  mouse.left = false;
+  mouse.right = false;
+};
+
+const changeDifficulty = button => {
+  game.setDifficulty(button.innerHTML.toLocaleLowerCase());
   initGame();
-}
+};
 //Prevent context menu (right click)
 document.addEventListener("contextmenu", event => event.preventDefault());
