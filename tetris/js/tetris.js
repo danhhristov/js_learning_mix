b4f41578f795d0c309bc1f34ba684f1ac455d8c2
Daniel Hristov <dhristov@live.co.uk>
2018-11-15 14:55:00 +0100

    new project: tetris

---
+++ tetris/js/tetris.js
@@ -0,0 +1,20 @@
+/**
+ $colors: (
+    i: cyan,
+    j: blue,
+    l: orange,
+    o: yellow,
+    s: green,
+    t: purple,
+    z: red,
+);
+
+$gpx-size: (
+  small: 10px,
+  medium: 20px,
+  large: 30px,
+  huge: 60px
+);
+ */
+$("#score").html("xxxXXXxxx");
+//instructions.onGameStart.addClass("hidden");
